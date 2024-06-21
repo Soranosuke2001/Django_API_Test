7f8514f992a244d14f6a88e894ec9a79f68c4e2a
Soranosuke2001 <sorazora1.4@gmail.com>
2024-06-21 11:56:16 +0900

    created custom api view

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.views import APIView
 
 from .models import BlogPost
 from .serializers import BlogPostSerializers
@@ -23,3 +24,18 @@ class BlogPostRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
   serializer_class = BlogPostSerializers
   lookup_field = "pk"
 
+
+class BlogPostList(APIView):
+  def get(self, request, format=None):
+    title = request.query_params.get("title", "")
+
+    if title:
+      blog_posts = BlogPost.objects.filter(title__icontains=title)
+    else:
+      blog_posts = BlogPost.objects.all()
+
+    serializer = BlogPostSerializers(blog_posts, many=True)
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+    
