6cb909e19aa7c5bf909fcc575119ee6029b6f2dd
Soranosuke2001 <sorazora1.4@gmail.com>
2024-06-21 11:38:24 +0900

    view to create a blog post

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from rest_framework.generics import ListCreateAPIView
+
+from .models import BlogPost
+from .serializers import BlogPostSerializers
+
+
 # Create your views here.
+class BlogPostListCreate(ListCreateAPIView):
+  queryset = BlogPost.objects.all()
+  serializer_class = BlogPostSerializers
+
+  
