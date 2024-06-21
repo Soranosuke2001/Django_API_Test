635f76b8206b0adabdef17e9bcb9bbd908cec723
Soranosuke2001 <sorazora1.4@gmail.com>
2024-06-21 11:52:18 +0900

    custom method to delete all blog posts at once

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from .models import BlogPost
@@ -11,10 +13,13 @@ class BlogPostListCreate(ListCreateAPIView):
   queryset = BlogPost.objects.all()
   serializer_class = BlogPostSerializers
 
+  def delete(self, request, *args, **kwargs):
+    BlogPost.objects.all().delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
   
 class BlogPostRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
   queryset = BlogPost.objects.all()
   serializer_class = BlogPostSerializers
   lookup_field = "pk"
 
-  
