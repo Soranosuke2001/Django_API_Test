4d9e0d7dbd951c1274a80a46a5bf4a2da6fdcfb7
Soranosuke2001 <sorazora1.4@gmail.com>
2024-06-21 11:34:37 +0900

    seralizer file when returning a JSON object, fields identified in this file will be returned

---
+++ mysite/api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from .models import BlogPost
+
+
+class BlogPostSerializers(serializers.ModelSerializer):
+  class Meta:
+    model = BlogPost
+    fields = ["id", "title", "content", "published_date"]
+
+    
