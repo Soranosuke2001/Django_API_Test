e3d3b3f6d8d04bcca5d0f9321058fdb8c7b30735
Soranosuke2001 <sorazora1.4@gmail.com>
2024-06-21 11:58:11 +0900

    added url to search

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import BlogPostListCreate, BlogPostRetrieveUpdateDestroy
+from . import views
 
 app_name = 'api'
 
 urlpatterns = [
-  path("blogposts/", BlogPostListCreate.as_view(), name="blogpost-view-create"),
-  path("blogposts/<int:pk>/", BlogPostRetrieveUpdateDestroy.as_view(), name="update"),
+  path("blogposts/", views.BlogPostListCreate.as_view(), name="blogpost-view-create"),
+  path("blogposts/<int:pk>/", views.BlogPostRetrieveUpdateDestroy.as_view(), name="update"),
+  path("blogposts/search/", views.BlogPostList.as_view(), name="search"),
 ]
