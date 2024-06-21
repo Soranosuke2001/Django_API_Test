cfd76a49f128014624fbaf1559ccf47e19e2de67
Soranosuke2001 <sorazora1.4@gmail.com>
2024-06-21 11:56:57 +0900

    removed unnecessary import

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
@@ -38,4 +36,3 @@ class BlogPostList(APIView):
 
     return Response(serializer.data, status=status.HTTP_200_OK)
 
-    
